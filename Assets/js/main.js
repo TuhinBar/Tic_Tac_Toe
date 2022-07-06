bf169d0830630958a21345d194f312bcf2da6e6d
Arian Shaikh <ariansk90@gmail.com>
2022-07-07 04:13:10 +0530

    added modal

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,1 +1,10 @@
 console.log("This is a tic tac toe fun game")
+let play = document.getElementById("play")
+// console.log(modal);
+let modal = document.getElementById("modal")
+
+play.addEventListener("click", () =>{
+    console.log("close the modal")
+    modal.classList.add("modal_close")
+
+});
