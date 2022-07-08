90d65147ab33602ab37090787b23449026276676
Arian Shaikh <ariansk90@gmail.com>
2022-07-09 02:34:29 +0530

    added tic and tac

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,3 +8,20 @@ play.addEventListener("click", () =>{
     modal.classList.add("modal_close")
 
 });
+turn = "1"
+boxes = document.getElementsByClassName('box')
+console.log(boxes)
+ Array.from(boxes).forEach(item => {
+    item.addEventListener('click', ()=>{
+        console.log("box clicked")
+        if (turn == "1"){
+            item.classList.add("fa-solid", "fa-xmark", "fa-5x")
+            turn = "0"
+        }
+        else{
+            item.classList.add("fa-regular", "fa-o", "fa-5x")
+            turn = "1"
+        }
+        
+    })
+ })
