4e28fda171bc0855012dfa1fc5af791030075cb9
Arian Shaikh <ariansk90@gmail.com>
2022-07-12 00:34:22 +0530

    dear hr

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -19,32 +19,55 @@ play.addEventListener("click", () =>{
     modal.classList.add("modal_close")
 
 });
+let clicked_box = [];
+let arr_x = [];
+let arr_o = [];
 turn = "1"
 boxes = document.getElementsByClassName('box')
 console.log(boxes)
 Array.from(boxes).forEach(item => {
     item.addEventListener('click', ()=>{
         console.log("box clicked")
-        if (turn == "1"){
-            item.classList.add("fa-solid", "fa-xmark", "fa-3x")
+        if (turn == "1" && clicked_box.includes(item.id) == false){
+            item.classList.add("fa-solid", "fa-xmark", "fa-3x");
+            arr_x.push(item.id)
+            clicked_box.push(item.id);
+            if (item.id == "box6" && arr_x.includes("box5") == true){
+                window.alert("won")
+            }
+
+
+
+
+
             turn = "0"
         }
-        else{
+        else if (turn == "0" && clicked_box.includes(item.id) == false){
             item.classList.add("fa-regular", "fa-o", "fa-3x")
+            arr_o.push(item.id);
+            clicked_box.push(item.id);
             turn = "1"
         }
         
     })
- })
+ });
+
 const reset_btn = document.getElementById("reset");
 reset_btn.addEventListener("click", () =>{
     Array.from(boxes).forEach(item =>{
         item.classList.remove("fa-solid", "fa-xmark", "fa-5x", "fa-regular", "fa-o", "fa-5x" 
         )})
     turn = "1"
+    arr_x = [];
+    arr_o = [];
+    clicked_box = [];
 
 })
 const exit_btn = document.getElementById("exit");
 exit_btn.addEventListener("click", () =>{
+    arr_x = [];
+    arr_o = [];
     window.location.reload()
 });
+console.log(arr_x);
+console.log(arr_o);
